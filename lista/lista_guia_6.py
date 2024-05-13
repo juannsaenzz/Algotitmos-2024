3884da29dac201dcaef3c26c3b9ebd6ccc4d6b49
juannsaenzz <juannsaenzz17@gmail.com>
2024-05-13 20:23:41 -0300

    practica

---
+++ lista/lista_guia_6.py
@@ -0,0 +1,17 @@
+
+#! EJERCICIO 6
+#! Dada una lista de superhéroes de comics, de los cuales se conoce su nombre,
+#! año aparición, casa de comic a la que pertenece (Marvel o DC) y biografía,
+#! implementar la funciones necesarias para poder realizar las siguientes actividades:
+
+#! a. eliminar el nodo que contiene la información de Linterna Verde;
+#! b. mostrar el año de aparición de Wolverine;
+#! c. cambiar la casa de Dr. Strange a Marvel;
+#! d. mostrar el nombre de aquellos superhéroes que en su biografía menciona la
+#! palabra “traje” o “armadura”;
+#! e. mostrar el nombre y la casa de los superhéroes cuya fecha de aparición sea
+#! anterior a 1963;
+#! f. mostrar la casa a la que pertenece Capitana Marvel y Mujer Maravilla;
+#! g. mostrar toda la información de Flash y Star-Lord;
+#! h. listar los superhéroes que comienzan con la letra B, M y S;
+#! i. determinar cuántos superhéroes hay de cada casa de comic.
