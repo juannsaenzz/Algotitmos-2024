0d95a3f82f5f86b1d8113eb9541eb44fbdfd7908
juannsaenzz <juannsaenzz17@gmail.com>
2024-05-30 10:48:12 -0300

    practica

--- cola/cola.py
+++ cola/cola.py
@@ -1,48 +1,29 @@
 
+
 class Queue:
 
-    def __init__(self): #! Inicia una cola
+    def __init__(self):
         self.__elements = []
 
-    def arrive(self, element): #! Agrega un elemento al final de la cola
+    def arrive(self, element):
         self.__elements.append(element)
 
-    def attention(self): #! Elimina y devuelve el elemento del frente de la cola
+    def attention(self):
         if len(self.__elements) > 0:
             return self.__elements.pop(0)
         else:
             return None
     
-    def size(self): #! Muestra la cantidad de elementos de la cola
+    def size(self):
         return len(self.__elements)
 
-    def on_front(self): #! Devuelve el valor del frente de la cola, sin eliminarlo
+    def on_front(self):
         if len(self.__elements) > 0:
             return self.__elements[0]
         else:
             return None
     
-    def move_to_end(self): #! Elimina el elemento del frente y lo inserta en el final de la cola
+    def move_to_end(self):
         element = self.attention()
         if element is not None:
             self.arrive(element)
-
-# cola_1 = Queue()
-
-# cola_1.arrive(1)
-# cola_1.arrive(2)
-# cola_1.arrive(3)
-# cola_1.arrive(4)
-# cola_1.arrive(5)
-# cola_1.attention()
-# cola_1.attention()
-# print(cola_1.size())
-# print(cola_1.on_front())
-# cola_1.move_to_end()
-# cola_1.move_to_end()
-# print()
-# for i in range(cola_1.size()):
-#     print(cola_1.on_front())
-#     cola_1.move_to_end()
-# print()
-# print(cola_1.size())
---
+++ hash/TP4_tabla_hash.py
@@ -0,0 +1,16 @@
+
+#! Escribir un algoritmo que permita utilizar tres tablas hash para guardar los
+#! datos de Pokémons, que contemple las siguientes actividades: 
+
+#! a. en la primera tabla hash la función hash debe ser sobre el tipo de Pokémon,
+#! en la segunda tabla la función hash deberá utilizar el ultimo dígito del número del
+#! Pokémon como clave y la tercera sera en base  a su nivel repartiéndolos en 10
+#! posiciones dentro de la tabla; 
+#! b. debe utilizar tablas hash abiertas con listas como estructura secundaria;
+#! c. si el Pokémon es de más de un tipo deberá cargarlo en cada uno de las tabla que
+#! indiquen estos tipos;
+#! d. deberá permitir cargar Pokémons de los cuales se dispone de su número, nombre,
+#! tipo/s, nivel.
+#! e. mostrar todos los Pokémons cuyos numeros terminan en 3, 7 y 9;
+#! f. mostrar todos los Pokémons cuyos niveles son multiplos de 2, 5 y 10;
+#! g. mostrar todos los Pokémons de los siguientes tipo: Acero, Fuego, Electrifico, Hielo
